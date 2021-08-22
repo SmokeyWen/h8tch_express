658388aa456c5faa680e0f341d4124cc0a46d1e8
SmokeyWen <835033838wen@gmail.com>
2021-08-21 18:34:32 -0700

    bug fixing on deleteAll

--- routes/task/del/index.js
+++ routes/task/del/index.js
@@ -30,7 +30,8 @@ router.delete('/', async(req, res) => {
         initDB.initDB(dbName, collectionName, function(instance, collection) {
             collection.deleteMany()
             .then( async (result) => {
-                // await instance.close(() => console.log('DELETE all tasks done. DB closed...'));
+                console.log('delete all result', result)
+                await instance.close(() => console.log('DELETE all tasks done. DB closed...'));
                 return res.status(200).json({msg : "All tasks deleted"})
             })
             .catch(err => console.log(err))
